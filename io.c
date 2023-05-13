8b3b6181b9e297b541042259819fd348727e97c2
Dreg <dreg@fr33project.org>
2023-05-13 14:37:32 +0200

    add MIT License header to src files

--- io.c
+++ io.c
@@ -1,3 +1,40 @@
+/*
+MIT License
+
+https://github.com/therealdreg/dregs-flex-bison-example
+
+Copyright (c) [2023] by David Reguera Garcia aka Dreg 
+dreg@fr33project.org
+https://www.fr33project.org 
+https://github.com/therealdreg
+TW @therealdreg
+
+Credits:
+- Original repo by Gergely Dévai ( @devaigergely81 ): 
+https://github.com/devaigergely81/flex-bison-example
+- Fork by Kókai Péter ( @Kokan ): 
+https://github.com/Kokan/flex-bison-example
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <string.h>
 
